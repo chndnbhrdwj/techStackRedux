3f2a622929328201f53f722049a99297c40932cf
Chandan Kumar <chandank@KETCHUP.local>
2019-05-06 21:51:54 +0100

    minor update

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -4,27 +4,22 @@ import { connect } from 'react-redux';
 import ListItem from './ListItem';
 
 class LibraryList extends Component {
- renderItem(library) {
-  return (
-   <ListItem 
-   library={library}
-   />
-  );
- }
- render() {
-  return (
-   <FlatList 
-   data={this.props.libraries}
-   renderItem={this.renderItem}
-   keyExtractor={(library) => library.id.toString()}
-   />
-  );
- }
+  renderItem(library) {
+    return <ListItem library={library} />;
+  }
+  render() {
+    return (
+      <FlatList
+        data={this.props.libraries}
+        renderItem={this.renderItem}
+        keyExtractor={library => library.id.toString()}
+      />
+    );
+  }
 }
 
-const mapStateToProps = state => { 
- return { libraries: state.libararies };
-};  
-
+const mapStateToProps = state => {
+  return { libraries: state.libraries };
+};
 
 export default connect(mapStateToProps)(LibraryList);
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,21 +3,19 @@ import { Text } from 'react-native';
 import { CardSection } from './common';
 
 export default class ListItem extends Component {
- render() {
-  const { titleStyle } = styles;
-  return (
-   <CardSection>
-    <Text style={titleStyle}>
-    {this.props.library.item.title}
-    </Text>
-   </CardSection>
-  );
- }
+  render() {
+    const { titleStyle } = styles;
+    return (
+      <CardSection>
+        <Text style={titleStyle}>{this.props.library.item.title}</Text>
+      </CardSection>
+    );
+  }
 }
 
 const styles = {
- titleStyle: {
-  fontSize: 18,
-  paddingLeft: 15 
- }
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15
+  }
 };
